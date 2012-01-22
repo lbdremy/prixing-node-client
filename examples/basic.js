af0890ab1847eb2e9ad887c49b25bbcc020f451f
Remy Loubradou <remyloubradou@gmail.com>
2012-01-22 18:48:02 +0100

    FIX: Remove my api key.

--- examples/basic.js
+++ examples/basic.js
@@ -2,7 +2,7 @@
 var prixing = require('./../main');
 
 // Create a Client
-var apikey = '4cef92e2efc64f6984c935f61dc032a3' // Put your own apikey
+var apikey = 'xxxxxx' // Put your own apikey
 var client = prixing.createClient(apikey);
 
 // Get a product using its EAN (bar code)
